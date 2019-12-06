ec822d455ab1bed8b40370378eee9e996dec27be
Michael Williams <manicdesigner@gmail.com>
2019-12-06 15:06:36 -0500

    update db by michelle

--- data/db.js
+++ data/db.js
@@ -10,7 +10,7 @@ module.exports = {
   remove,
   findPostComments,
   findCommentById,
-  insertComment,
+  insertComment
 };
 
 function find() {
@@ -18,7 +18,9 @@ function find() {
 }
 
 function findById(id) {
-  return db('posts').where({ id: Number(id) });
+  return db('posts')
+    .where({ id: Number(id) })
+    .first();
 }
 
 function insert(post) {
