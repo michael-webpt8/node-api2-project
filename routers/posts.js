5a2319d5411818572f3abc2d810bc061cfc36af7
Mikey Williams <manicdesigner@gmail.com>
2019-12-07 11:49:23 -0500

    mod posts file

--- routers/posts.js
+++ routers/posts.js
@@ -35,7 +35,7 @@ router.post('/', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        error: 'There was an error whiel saving the post to the database'
+        error: 'There was an error while saving the post to the database'
       });
     });
 });
