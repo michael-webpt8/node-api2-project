43341b6285edaf499b340b2fd53fd46e38e121b7
Michael Williams <manicdesigner@gmail.com>
2019-12-07 13:07:11 -0500

    checking the array length is false to pass data or not

--- routers/comments.js
+++ routers/comments.js
@@ -2,23 +2,25 @@ const express = require('express');
 const db = require('../data/db');
 
 const router = express.Router({
-    mergeParams: true
-})
+  mergeParams: true
+});
 
-router.get('/', (req, res) => {
-    db.find()
-        .then(data => {
-            if (data) {
-                res.json(data);
-            } else {
-                res.json(404).json({ message: "The post with the specified ID does not exist." })
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-
-            })
-        })
-})
+router.get('/:id/comments', (req, res) => {
+  db.findPostComments(req.params.id)
+    .then(data => {
+      if (data.length) {
+        res.json(data);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: 'server error, according to Mikey' });
+    });
+});
 
 module.exports = router;
