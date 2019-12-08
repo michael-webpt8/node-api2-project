bb168cc6eb5721d2077f3b0190690a2280b131ec
Mikey Williams <manicdesigner@gmail.com>
2019-12-08 17:27:09 -0500

    update delete route

--- routers/posts.js
+++ routers/posts.js
@@ -64,7 +64,7 @@ router.post('/', (req, res) => {
 router.get('/:id', (req, res) => {
   db.findById(req.params.id)
     .then(post => {
-      if (post) {
+      if (post.length) {
         res.status(200).json(post);
       } else {
         res
@@ -118,6 +118,24 @@ router.put('/:id', (req, res) => {
               .json({ error: 'The post information could not be retrieved.' });
           })
       }
+    })
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  console.log('id', id);
+  db.findById(id)
+    .then(post => {
+      console.log(post)
+      if (post.length > 0) {
+        db.remove(post);
+        res.status(400).json({ message: "message was deleted" });
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "The post could not be removed" })
     })
 });
 
