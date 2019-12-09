29631309fd2c51403def7eeaf560a80b7d0e56dc
Mikey Williams <manicdesigner@gmail.com>
2019-12-09 11:39:27 -0500

    add client and start of trying to do stretch goal

---
+++ client/src/components/Posts.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function Posts() {
+    const [post, setPost] = useState();
+
+    useE
+
+    return (
+        <>
+
+        </>
+    )
+}
+export default Posts;
