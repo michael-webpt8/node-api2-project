7c397317e79fb77227948ad38a18f3e1af2f0388
Michael Williams <manicdesigner@gmail.com>
2019-12-13 12:30:45 -0500

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const postRouter = require('./routers/posts');
+const cors = require('cors');
 
 const server = express();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/posts', postRouter);
 
