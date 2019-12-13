ae7c87be8511e989b999e9fc7e2f8c10fef20c92
Michael Williams <manicdesigner@gmail.com>
2019-12-12 19:19:40 -0500

    dotenv

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const postRouter = require('./routers/posts');
 
 const server = express();
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
 
 server.use(express.json());
 
 server.use('/api/posts', postRouter);
 
-server.listen(4000, () => {
+server.listen(port, host, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
